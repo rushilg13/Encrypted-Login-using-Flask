32e7d5b403f833ad1e4970f33413bfb655c35437
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-01-27 19:58:03 +0530

    Fixed

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Email, EqualTo, length
-import email_validator
+from wtforms.fields.html5 import EmailField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Cant_say'
@@ -10,7 +10,7 @@ app.config['SECRET_KEY'] = 'Cant_say'
 class inputForm(Form):
     fname = StringField('fname', validators=[InputRequired()])
     lname = StringField('lname', validators=[InputRequired()])
-    email = StringField('email', validators=[InputRequired(), Email()])
+    email = EmailField('email', validators=[InputRequired(), Email()])
     pass1 = PasswordField('pass1', validators=[InputRequired(), length(min=6, max=20)])
     cpass = PasswordField('cpass', validators=[InputRequired(), length(min=6, max=20), EqualTo('pass1', message='Passwords must match')])
 
