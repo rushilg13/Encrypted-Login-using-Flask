29561a3a52c9f9d3a3b1e3edfeaef9ea9b78817d
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-01-27 19:41:31 +0530

    Adding form

--- login.py
+++ login.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_wtf import Form
-from wtforms import StringField
-from wtforms.validators import *
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, EqualTo, length
 
 app = Flask(__name__)
-@app.route('/')
+app.config['SECRET_KEY'] = 'Cant_say'
+
+class inputForm(Form):
+    fname = StringField('fname', validators=[InputRequired()])
+    lname = StringField('lname', validators=[InputRequired()])
+    email = StringField('email', validators=[InputRequired(), Email()])
+    pass1 = PasswordField('pass1', validators=[InputRequired(), length(min=6, max=20)])
+    cpass = PasswordField('cpass', validators=[InputRequired(), length(min=6, max=20), EqualTo('pass1', message='Passwords must match')])
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
+    form = inputForm()
+    if request.method=="POST":
+        fname = form.fname.data
+        lname = form.lname.data
+        email = form.email.data
+        pass1 = form.pass1.data
+        cpass = form.cpass.data
+        print(fname, lname, email, pass1, cpass)
+        return redirect('/')
     return render_template("home.html")
 
 if __name__ == "__main__":
