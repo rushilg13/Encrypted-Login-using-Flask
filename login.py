f0edff4fe1ea5990f0fc1d6971832105633226e3
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-01-28 14:04:43 +0530

    Login page Completed

--- login.py
+++ login.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import Form
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, validators, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 from wtforms.fields.html5 import EmailField
 from flask_pymongo import pymongo
-import cryptocode
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Cant_say'
@@ -39,25 +38,38 @@ def signup():
         if form.validate_on_submit():
             return("Submitted")
         else:
-            ciphertext = cryptocode.encrypt(pass1,"mysecretkey")
-            # decrypttext = cryptocode.decrypt(ciphertext, "mysecretkey")
-            user_collection.insert_one({'First Name': fname, 'Last Name': lname, 'Email': email, 'Password': ciphertext})
+            new=''
+            for s in pass1:
+                n = ((ord(s) + 237)**2)%26
+                new+=chr(n)
+            user_collection.insert_one({'First Name': fname, 'Last Name': lname, 'Email': email, 'Password': new})
         return "Logged in"
     return render_template("signup.html", form=form)
 
-@app.route('/login', method=['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form_login = inputFormlogin()
     if request.method=="POST":
         email = form_login.email.data
         pass1 = form_login.pass1.data
-    if form_login.validate_on_submit():
-        return("Submitted")
-    else:
-        ciphertext = cryptocode.encrypt(pass1,"mysecretkey")
-        # decrypttext = cryptocode.decrypt(ciphertext, "mysecretkey")
-        return "Logged in"
-    return render_template("signup.html", form_login=form_login)
+        if form_login.validate_on_submit():
+            return("Submitted")
+        else:
+            new=''
+            for s in pass1:
+                n = ((ord(s) + 237)**2)%26
+                new+=chr(n)
+            filter_dict = {"Email": email, "Password": new}
+            # print(filter_dict)
+            if user_collection.count_documents(filter_dict):
+                print("item is existed")
+                flash("item is existed")
+                return "Logged in"
+            else:
+                print("item is not existed")
+                flash('Invalid Credentials')
+                return redirect('/login')
+    return render_template("login.html", form_login=form_login)
 
 
 
