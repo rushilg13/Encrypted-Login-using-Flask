d02a1a3e16fe3407e69cc36d804fa8f2c5f6c932
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-01-28 13:12:38 +0530

    Added Encryption

--- login.py
+++ login.py
@@ -4,6 +4,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, validators, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 from wtforms.fields.html5 import EmailField
+from flask_pymongo import pymongo
+import cryptocode
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Cant_say'
@@ -15,6 +17,10 @@ class inputForm(Form):
     pass1 = PasswordField('pass1', validators=[DataRequired()])
     sub = SubmitField('Sign Up')
 
+CONNECTION_STRING = "mongodb+srv://VIT_Admin:pizza@vitdiaries.tpuku.mongodb.net/CouponShare?retryWrites=true&w=majority"
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client.get_database('CouponShare')
+user_collection = pymongo.collection.Collection(db, 'Users')
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
@@ -24,11 +30,13 @@ def home():
         lname = form.lname.data
         email = form.email.data
         pass1 = form.pass1.data
-        print(fname, lname, email, pass1)
+        # print(fname, lname, email, pass1)
         if form.validate_on_submit():
             return("Submitted")
         else:
-            pass
+            ciphertext = cryptocode.encrypt(pass1,"mysecretkey")
+            # decrypttext = cryptocode.decrypt(ciphertext, "mysecretkey")
+            user_collection.insert_one({'First Name': fname, 'Last Name': lname, 'Email': email, 'Password': ciphertext})
         return "Logged in"
     return render_template("home.html", form=form)
 
