359caa43749373e80ceefdeb9252e9b4687cc72e
rushilg13 <rushil.goel2019@vitstudent.ac.in>
2021-01-28 12:47:44 +0530

    Fixed form

--- login.py
+++ login.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, request, redirect
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email, EqualTo, length
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, validators, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 from wtforms.fields.html5 import EmailField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Cant_say'
 
 class inputForm(Form):
-    fname = StringField('fname', validators=[InputRequired()])
-    lname = StringField('lname', validators=[InputRequired()])
-    email = EmailField('email', validators=[InputRequired(), Email()])
-    pass1 = PasswordField('pass1', validators=[InputRequired(), length(min=6, max=20)])
-    cpass = PasswordField('cpass', validators=[InputRequired(), length(min=6, max=20), EqualTo('pass1', message='Passwords must match')])
+    fname = StringField('fname', validators=[DataRequired()])
+    lname = StringField('lname', validators=[DataRequired()])
+    email = EmailField('email', validators=[DataRequired(), Email()])
+    pass1 = PasswordField('pass1', validators=[DataRequired()])
+    sub = SubmitField('Sign Up')
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -23,9 +24,12 @@ def home():
         lname = form.lname.data
         email = form.email.data
         pass1 = form.pass1.data
-        cpass = form.cpass.data
-        print(fname, lname, email, pass1, cpass)
-        return redirect('/')
+        print(fname, lname, email, pass1)
+        if form.validate_on_submit():
+            return("Submitted")
+        else:
+            pass
+        return "Logged in"
     return render_template("home.html", form=form)
 
 if __name__ == "__main__":
